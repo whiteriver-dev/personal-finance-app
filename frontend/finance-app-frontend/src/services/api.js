3cbac2b705e9bb00f9b8a88897ed3dfc5545a8a1
Igor Dubik <whiteriver.devprojects@gmail.com>
2025-02-10 14:28:09 +1100

    minor fixes

--- frontend/finance-app-frontend/src/services/api.js
+++ frontend/finance-app-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api'; //Backend URL
+export const API_URL = 'http://localhost:8080/api'; //Backend URL
 
 export const registerApi = async (userData) => {
     return axios.post(`http://127.0.0.1:8000/api/auth/register/`, userData); // POST request to Register endpoint
